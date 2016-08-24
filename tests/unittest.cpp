c7d87acc307c277d8bbd062fa5e6d814851af360
Krystian Dużyński <krystian.duzynski@gmail.com>
2016-08-24 21:25:28 +0200

    tests

--- tests/unittest.cpp
+++ tests/unittest.cpp
@@ -1,7 +1,18 @@
 #include <gtest/gtest.h>
 
+#include "../structpack.h"
+
+#define ASSERT_EQ_ARRAY(a1,a2,len) \
+	{ for (int i = 0; i < len; i++) { ASSERT_EQ((uint8_t)a2[i], (uint8_t)a1[i]); } }
+
+uint8_t buf[100];
+
 TEST(structpack, test1)
 {
+	int r = StructPack::pack(buf, 100, "L", 123);
+
+	ASSERT_EQ(r, 4);
+	ASSERT_EQ_ARRAY(buf, "\x7b\x00\x00\x00", r);
 }
 
 int main(int argc, char **argv)
